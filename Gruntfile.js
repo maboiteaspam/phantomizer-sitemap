b2f3e767d2ae4b02ff16d156ddd58a5813d329fc
clement <maboiteaspam@wanadoo.fr>
2014-02-06 15:42:20 +0800

    Code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,81 +1,81 @@
 module.exports = function(grunt) {
 
 
-    var wrench = require('wrench'),
-        util = require('util');
+  var wrench = require('wrench'),
+    util = require('util');
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        docco: {
-            debug: {
-                src: [
-                    'tasks/build.js'
-                ],
-                options: {
-                    layout:'linear',
-                    output: 'documentation/'
-                }
-            }
-        },
-        'gh-pages': {
-            options: {
-                base: '.',
-                add: true
-            },
-            src: ['documentation/**']
-        },
-        release: {
-            options: {
-                bump: true,
-                add: false,
-                commit: false,
-                npm: false,
-                npmtag: true,
-                tagName: '<%= version %>',
-                github: {
-                    repo: 'maboiteaspam/phantomizer-sitemap',
-                    usernameVar: 'GITHUB_USERNAME',
-                    passwordVar: 'GITHUB_PASSWORD'
-                }
-            }
-        },
-        'phantomizer-sitemap': {
-            options: {
-            },
-            "test":{
-                "options":{
-                    routing:[
-                        {
-                            "template":"/category.htm",
-                            "test_url":"/some/category.htm",
-                            "tests":[
-                                "tests/module-name"
-                            ],
-                            "urls_file":"data/collection-of-urls.json"
-                        },
-                        {
-                            "template":"/templated-page.htm",
-                            "tests":[
-                                "tests/module-name"
-                            ],
-                            sitemap:false
-                        }
-                    ]
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    docco: {
+      debug: {
+        src: [
+          'tasks/build.js'
+        ],
+        options: {
+          layout:'linear',
+          output: 'documentation/'
         }
-    });
-    grunt.loadNpmTasks('grunt-docco');
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-release');
-    grunt.loadTasks('tasks');
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: '.',
+        add: true
+      },
+      src: ['documentation/**']
+    },
+    release: {
+      options: {
+        bump: true,
+        add: false,
+        commit: false,
+        npm: false,
+        npmtag: true,
+        tagName: '<%= version %>',
+        github: {
+          repo: 'maboiteaspam/phantomizer-sitemap',
+          usernameVar: 'GITHUB_USERNAME',
+          passwordVar: 'GITHUB_PASSWORD'
+        }
+      }
+    },
+    'phantomizer-sitemap': {
+      options: {
+      },
+      "test":{
+        "options":{
+          routing:[
+            {
+              "template":"/category.htm",
+              "test_url":"/some/category.htm",
+              "tests":[
+                "tests/module-name"
+              ],
+              "urls_file":"data/collection-of-urls.json"
+            },
+            {
+              "template":"/templated-page.htm",
+              "tests":[
+                "tests/module-name"
+              ],
+              sitemap:false
+            }
+          ]
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-docco');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadTasks('tasks');
 
-    grunt.registerTask('cleanup-grunt-temp', [],function(){
-        wrench.rmdirSyncRecursive(__dirname + '/.grunt', !true);
-        wrench.rmdirSyncRecursive(__dirname + '/documentation', !true);
-    });
-    grunt.registerTask('default', ['docco','gh-pages', 'cleanup-grunt-temp']);
-    grunt.registerTask('sitemap', ['phantomizer-sitemap']);
+  grunt.registerTask('cleanup-grunt-temp', [],function(){
+    wrench.rmdirSyncRecursive(__dirname + '/.grunt', !true);
+    wrench.rmdirSyncRecursive(__dirname + '/documentation', !true);
+  });
+  grunt.registerTask('default', ['docco','gh-pages', 'cleanup-grunt-temp']);
+  grunt.registerTask('sitemap', ['phantomizer-sitemap']);
 
 };
--- tasks/build.js
+++ tasks/build.js
@@ -2,70 +2,70 @@
 
 module.exports = function(grunt) {
 
-    var wrench = require('wrench'),
-        util = require('util');
-    //-
-    var ph_libutil  = require("phantomizer-libutil");
-    var path        = require("path");
-    var fs          = require("fs");
+  var wrench = require('wrench'),
+    util = require('util');
+  //-
+  var ph_libutil  = require("phantomizer-libutil");
+  var path        = require("path");
+  var fs          = require("fs");
 
-    var router_factory    = ph_libutil.router;
+  var router_factory    = ph_libutil.router;
 
-    grunt.registerMultiTask("phantomizer-sitemap", "Sitemap generator for phantomizer app", function(){
+  grunt.registerMultiTask("phantomizer-sitemap", "Sitemap generator for phantomizer app", function(){
 
-        var options = this.options({
-            routing:null,
-            target_path:"export/",
-            file_name:"sitemap.xml",
-            base_url:"http://localhost/"
-        });
-        var config = grunt.config();
-
-        var router = new router_factory(options.routing || config.routing);
-        router.load(function(){
-
-            var sitemap_fn = options.target_path+"/"+options.file_name;
-            if( fs.existsSync(sitemap_fn) ) fs.unlinkSync(sitemap_fn)
-
-            var output = "";
-            var eol = "\n";
-            var tab = "\t";
-            var base_url = options.base_url.substr(-1)=="/"?options.base_url:options.base_url+"/";
-
-            var meta_urls = router.collect_meta_urls();
-            for( var n in meta_urls ){
-                var meta = meta_urls[n].meta;
-                if( meta.sitemap != false &&  meta.export != false ){
-                    var last_mod = meta.sitemap&&meta.sitemap.last_mod || meta.last_mod || false;
-                    var changefreq = meta.sitemap&&meta.sitemap.changefreq || meta.changefreq || false;
-                    var priority = meta.sitemap&&meta.sitemap.priority || meta.priority || false;
-
-                    var url = meta_urls[n].url;
-                    url = url.substr(0,1)=="/"?url.substr(1):url;
-                    url = base_url + url;
-
-                    output += tab + '<url>' + eol;
-                    output += tab + tab + '<loc>'+ url +'</loc>' + eol;
-                    if( last_mod !== false )
-                        output += tab + tab + '<lastmod>'+last_mod+'</lastmod>' + eol;
-                    if( changefreq !== false )
-                        output += tab + tab + '<changefreq>'+changefreq+'</changefreq>' + eol;
-                    if( priority !== false )
-                        output += tab + tab + '<priority>'+priority+'</priority>' + eol;
-                    output += tab + '</url>' + eol;
-                }
-            }
-
-            if( output != "" ){
-                output += "</urlset>";
-                output = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + eol +output;
-                output = '<?xml version="1.0" encoding="UTF-8"?>' + eol +output;
-                wrench.mkdirSyncRecursive(options.target_path, "0777");
-                fs.writeFileSync(sitemap_fn, output);
-            }
-            grunt.log.ok();
-        })
+    var options = this.options({
+      routing:null,
+      target_path:"export/",
+      file_name:"sitemap.xml",
+      base_url:"http://localhost/"
     });
+    var config = grunt.config();
+
+    var router = new router_factory(options.routing || config.routing);
+    router.load(function(){
+
+      var sitemap_fn = options.target_path+"/"+options.file_name;
+      if( fs.existsSync(sitemap_fn) ) fs.unlinkSync(sitemap_fn)
+
+      var output = "";
+      var eol = "\n";
+      var tab = "\t";
+      var base_url = options.base_url.substr(-1)=="/"?options.base_url:options.base_url+"/";
+
+      var meta_urls = router.collect_meta_urls();
+      for( var n in meta_urls ){
+        var meta = meta_urls[n].meta;
+        if( meta.sitemap != false &&  meta.export != false ){
+          var last_mod = meta.sitemap&&meta.sitemap.last_mod || meta.last_mod || false;
+          var changefreq = meta.sitemap&&meta.sitemap.changefreq || meta.changefreq || false;
+          var priority = meta.sitemap&&meta.sitemap.priority || meta.priority || false;
+
+          var url = meta_urls[n].url;
+          url = url.substr(0,1)=="/"?url.substr(1):url;
+          url = base_url + url;
+
+          output += tab + '<url>' + eol;
+          output += tab + tab + '<loc>'+ url +'</loc>' + eol;
+          if( last_mod !== false )
+            output += tab + tab + '<lastmod>'+last_mod+'</lastmod>' + eol;
+          if( changefreq !== false )
+            output += tab + tab + '<changefreq>'+changefreq+'</changefreq>' + eol;
+          if( priority !== false )
+            output += tab + tab + '<priority>'+priority+'</priority>' + eol;
+          output += tab + '</url>' + eol;
+        }
+      }
+
+      if( output != "" ){
+        output += "</urlset>";
+        output = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + eol +output;
+        output = '<?xml version="1.0" encoding="UTF-8"?>' + eol +output;
+        wrench.mkdirSyncRecursive(options.target_path, "0777");
+        fs.writeFileSync(sitemap_fn, output);
+      }
+      grunt.log.ok();
+    })
+  });
 
 
 };
